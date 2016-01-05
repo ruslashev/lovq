c3228a2d291a47065d66a983193aa628fb2399aa
Ruslan Akbashev <ruslashev@gmail.com>
2016-01-05 04:43:34 +0300

    ok lets go

--- main.lua
+++ main.lua
@@ -15,8 +15,7 @@ function dot_product(vec1, vec2)
 end
 
 function mat_vec_mult(mat, vec)
-  return
-  { dot_product(mat[1], vec)
+  return { dot_product(mat[1], vec)
   , dot_product(mat[2], vec)
   , dot_product(mat[3], vec)
   , dot_product(mat[4], vec) }
@@ -46,26 +45,39 @@ function mat_mat_mult(mat1, mat2)
     } }
 end
 
+function vec_length(vec)
+  return math.sqrt(vec[1] * vec[1]
+  + vec[2] * vec[2]
+  + vec[3] * vec[3]
+  + vec[4] * vec[4])
+end
+
+function vec_normalize(vec)
+  if vec_length(vec) == 0 then
+    return { 0, 0, 0, 0 }
+  else
+    return { vec[1] / vec_length(vec)
+    , vec[2] / vec_length(vec)
+    , vec[3] / vec_length(vec)
+    , vec[4] / vec_length(vec) }
+  end
+end
+
+function vec_sub(vec1, vec2)
+  return { vec1[1] - vec2[1]
+  , vec1[2] - vec2[2]
+  , vec1[3] - vec2[3]
+  , vec1[4] - vec2[4] }
+end
+
+function lookAt(eye, center, up)
+  local f = vec_normalize(vec_sub(center, eye))
+end
+
 function love.load()
-  mat_t =
-  {
-    {1, 0, 0, 1},
-    {0, 1, 0, 2},
-    {0, 0, 1, 3},
-    {0, 0, 0, 1}
-  }
-  mat_s =
-  {
-    {2, 0, 0, 0},
-    {0, 2, 0, 0},
-    {0, 0, 2, 0},
-    {0, 0, 0, 1}
-  }
-  mat = mat_mat_mult(mat_t, mat_s)
-  print( mat[1][1], mat[1][2], mat[1][3], mat[1][4] )
-  print( mat[2][1], mat[2][2], mat[2][3], mat[2][4] )
-  print( mat[3][1], mat[3][2], mat[3][3], mat[3][4] )
-  print( mat[4][1], mat[4][2], mat[4][3], mat[4][4] )
+  vec = {1, 1, 1, 1}
+  vec = vec_normalize(vec)
+  print(vec[1], vec[2], vec[3], vec[4])
 end
 
 function love.update(dt)
